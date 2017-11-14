28cfb8ab59b79d780892efb8001db2e7d4c7e426
Jordan Lapid <jordanlapid@gmail.com>
2017-11-13 21:28:45 -0800

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+USE `burgers_db`;
+
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Chipotle Black Bean', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Turkey Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Bacon Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Salmon Burger', false, CURRENT_TIMESTAMP);
